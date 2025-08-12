d4772dd2ce0df261f498d2f72a064545b20ee249
Ritesh Raj <rajritesh03@gmail.com>
2025-08-13 01:40:42 +1000

    wip: commented

--- src/components/task-table/table-filter-advanced.tsx
+++ src/components/task-table/table-filter-advanced.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+// import { Button } from '@/components/ui/button';
+// import { Card, CardContent } from '@/components/ui/card';
 import { UIFilterGroups } from '@/model/table-filter';
 import { Task } from '@/model/task';
 import { Table } from '@tanstack/react-table';
-import { Fragment } from 'react';
+// import { Fragment } from 'react';
 
 interface DataTableFilterAdvanceProps {
   table: Table<Task>;
@@ -44,60 +44,61 @@ function getFilterGroupGridRowSpanStyle(filterGroups: UIFilterGroups, filterGrou
 }
 
 export function DataTableFilterAdvanced({ table }: DataTableFilterAdvanceProps) {
-  return (
-    <div>
-      <Card>
-        <CardContent>
-          <div className='grid grid-cols-9 gap-1'>
-            {filterGroups.filterGroups.map((filterGroup, filterGroupIndex) => (
-              <Fragment key={filterGroup.name}>
-                <div className='col-start-1' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
-                  <p>{filterGroup.name}</p>
-                </div>
+  return <div>Advanced Filter</div>;
 
-                {filterGroup.filters.map((filter, FilterIndex) => (
-                  <Fragment key={FilterIndex}>
-                    <div className='col-start-2' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
-                      {filterGroup.filterListAndOr && FilterIndex == 0 && (
-                        <>
-                          <p>And/Or</p>
-                          <Button>{filterGroup.filterListAndOr}</Button>
-                        </>
-                      )}
-                    </div>
-                    <div className='col-start-3'>
-                      {FilterIndex == 0 && <p>Field</p>}
-                      <Button>{filter.field}</Button>
-                    </div>
-                    <div>
-                      {FilterIndex == 0 && <p>Operator</p>}
-                      <Button>{filter.operator}</Button>
-                    </div>
-                    <div>
-                      {FilterIndex == 0 && <p>Value</p>}
-                      <Button>{'value' in filter ? filter.value : ''}</Button>
-                    </div>
-                    <div>
-                      {FilterIndex == 0 && <p>And/Or</p>}
-                      <Button disabled={filter.andOr === false}>{filter.andOr}</Button>
-                    </div>
-                    <Button>+</Button>
-                    <Button>-</Button>
-                  </Fragment>
-                ))}
-                <div className='col-start-9' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
-                  <Button>x</Button>
-                </div>
-                {filterGroupIndex !== filterGroups.length - 1 && (
-                  <div className='col-span-full'>
-                    <hr />
-                  </div>
-                )}
-              </Fragment>
-            ))}
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Card>
+  //       <CardContent>
+  //         <div className='grid grid-cols-9 gap-1'>
+  //           {filterGroups.filterGroups.map((filterGroup, filterGroupIndex) => (
+  //             <Fragment key={filterGroup.name}>
+  //               <div className='col-start-1' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
+  //                 <p>{filterGroup.name}</p>
+  //               </div>
+  //               {filterGroup.filters.map((filter, FilterIndex) => (
+  //                 <Fragment key={FilterIndex}>
+  //                   <div className='col-start-2' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
+  //                     {filterGroup.filterListAndOr && FilterIndex == 0 && (
+  //                       <>
+  //                         <p>And/Or</p>
+  //                         <Button>{filterGroup.filterListAndOr}</Button>
+  //                       </>
+  //                     )}
+  //                   </div>
+  //                   <div className='col-start-3'>
+  //                     {FilterIndex == 0 && <p>Field</p>}
+  //                     <Button>{filter.field}</Button>
+  //                   </div>
+  //                   <div>
+  //                     {FilterIndex == 0 && <p>Operator</p>}
+  //                     <Button>{filter.operator}</Button>
+  //                   </div>
+  //                   <div>
+  //                     {FilterIndex == 0 && <p>Value</p>}
+  //                     <Button>{'value' in filter ? filter.value : ''}</Button>
+  //                   </div>
+  //                   <div>
+  //                     {FilterIndex == 0 && <p>And/Or</p>}
+  //                     <Button disabled={filter.andOr === false}>{filter.andOr}</Button>
+  //                   </div>
+  //                   <Button>+</Button>
+  //                   <Button>-</Button>
+  //                 </Fragment>
+  //               ))}
+  //               <div className='col-start-9' style={{ ...getFilterGroupGridRowSpanStyle(filterGroups, filterGroupIndex) }}>
+  //                 <Button>x</Button>
+  //               </div>
+  //               {filterGroupIndex !== filterGroups.length - 1 && (
+  //                 <div className='col-span-full'>
+  //                   <hr />
+  //                 </div>
+  //               )}
+  //             </Fragment>
+  //           ))}
+  //         </div>
+  //       </CardContent>
+  //     </Card>
+  //   </div>
+  // );
 }
